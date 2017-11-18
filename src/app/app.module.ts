4b8440de5e6bae75815c3207aef31997f2082190
Toluwase <wase932@gmail.com>
2017-11-18 13:37:28 -0700

    Interface. Completed design of user profile badge

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ProfileCardHeadComponent } from './profile-card-head/profile-card-head.component';
+import { RatingStarComponent } from './rating-star/rating-star.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProfileCardHeadComponent,
+    RatingStarComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/profile-card-head/profile-card-head.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-profile-card-head',
+  templateUrl: './profile-card-head.component.html',
+  styleUrls: ['./profile-card-head.component.css']
+})
+export class ProfileCardHeadComponent implements OnInit {
+  
+  constructor() { }
+
+  ngOnInit() {
+    // tolu: number = 0;
+  }
+
+}
